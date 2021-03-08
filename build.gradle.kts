094ad2da6c8ce87f0918e030f8836376c029f069
dreamhopping <71222289+dreamhopping@users.noreply.github.com>
2021-03-08 18:46:26 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,15 +10,3 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
-
-dependencies {
-    testImplementation(kotlin("test-junit"))
-}
-
-tasks.test {
-    useJUnit()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
