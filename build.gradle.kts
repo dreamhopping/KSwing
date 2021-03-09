257dd4ea27c7715d946394ff6cdfee4e31008295
dreamhopping <71222289+dreamhopping@users.noreply.github.com>
2021-03-09 21:26:29 +0000

    Fix buildscript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     kotlin("jvm") version "1.4.31"
 }
 
 group = "dev.dreamhopping.kswing"
 version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
 
 tasks.withType<Test> { enabled = false }
